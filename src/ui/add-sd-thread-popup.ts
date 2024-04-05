52d57a081db7c7eea9e2d517cbb4cbe40f011f34
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-04-05 23:38:11 +0200

    Add sd thread popup

---
+++ src/ui/add-sd-thread-popup.ts
@@ -0,0 +1,33 @@
+import {addThreadIdToLocalStorage, getDataFromLocalStorage} from "../helper-functions";
+
+export function addSdPopup(currentThreadId: string | null){
+    console.log("no thread ids found")
+
+
+    const popupBoxNewThread = `
+                    <div class="popup_box_container" id="dbInfo_popup_box">
+                        <div class='popup_box show'>
+                            <a class='popup_box_close tooltip-delayed' href='#' onclick='$(\"#dbInfo_popup_box\").remove()'>&nbsp</a>
+                            <div class='popup_box_content'>
+                              <div class='center'>
+                                <h2>Handelt es sich bei dem Thread um die Sd Tabelle?</h2>
+                                <input id="safeThreadAsSd" type="button" value="Ja, bitte abspeichern" class="btn" style="margin-top: 5px;">
+                              </div>
+                            </div>
+                        </div>
+                        <div class='fader'></div>
+                    </div>
+    `
+    $('#ds_body')[0].insertAdjacentHTML('beforeend', popupBoxNewThread)
+    $("#safeThreadAsSd").on("click", function () {
+        const edit_post_id = $(".post > a").attr("name")
+        if (edit_post_id !== undefined) {
+            addThreadIdToLocalStorage(currentThreadId, edit_post_id);
+            console.log(getDataFromLocalStorage("threadIds"))
+            $("#dbInfo_popup_box").remove()
+        } else {
+            console.error("edit_post_id is undefined")
+        }
+
+    });
+}

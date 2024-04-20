6c1d04147cdf87973561858c7fa57948279f8cae
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-04-20 22:21:31 +0200

    Refactor matching link

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  Automatisiert das Organisieren von Standdeffs in Die Stämme
 // @author       TheHebel97
-// @match        https://*.die-staemme.de/game.php?village=*&screen=place&mode=call&target=*
+// @match        https://*.die-staemme.de/game.php?village=*&screen=place&mode=call*
 // @match        https://*.die-staemme.de/game.php?village=*&screen=forum*
 // @match        https://*.die-staemme.de/game.php?*village=*&screen=settings*
 // @grant        GM_getValue

884fd56b43611a1194c1f5820817f42c53ae5956
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-05-08 18:03:10 +0200

    Refactor types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,9 @@
 import {Unit} from "./tw-types";
 
-//noch renamen
-export type sdInquiry = {
-    coords: string;
-    amount: number;
-    playerName: string | undefined;
-    comment: string | undefined;
-    dateFrom: number | undefined;
-    dateUntil: number | undefined;
-}
 
-export type ThreadData = {
-    threadName: string;
-    forumName: string;
-    forumId: string;
-    sdPostId: string;
-    activeBunkerInquiry: sdInquiry[];
-    stateOfSdTable: rowSdTable[];
-    packetSent: packetSent[];
-
+export type LocalStorageData = {
+    generalSettings: generalSettings,
+    threads: Threads
 }
 
 export type generalSettings = {
@@ -28,12 +13,37 @@ export type generalSettings = {
     sdGroupId: string,
     groupData: groupData[]
 }
-
 export type groupData = {
     id: string;
     name: string;
 }
 
+export type Threads = {
+    [threadId: string]: ThreadData
+}
+export type ThreadData = {
+    threadName: string;
+    forumName: string;
+    forumId: string;
+    sdPostId: string;
+    activeBunkerInquiry: sdInquiry[];
+    stateOfSdTable: sdTableData;
+    packages: packages;
+
+}
+
+export type sdInquiry = {
+    coords: string;
+    amount: number;
+    playerName: string | undefined;
+    comment: string | undefined;
+    dateFrom: number | undefined;
+    dateUntil: number | undefined;
+}
+
+export type sdTableData = {
+    [internalId: string]: rowSdTable
+}
 export type rowSdTable = {
     internalId: number;
     coords: string;
@@ -45,17 +55,14 @@ export type rowSdTable = {
     dateUntil: number;
 }
 
-export type packetSent = {
-    internalId: number;
-    coords: string;
+export type packages = {
+    [internalId: string]: packageSent
+
+}
+export type packageSent = {
     amount: number;
 }
 
-export type Threads = {
-    [threadId: string]: ThreadData
-}
 
-export type LocalStorageData = {
-    generalSettings: generalSettings,
-    threads: Threads
-}
+
+

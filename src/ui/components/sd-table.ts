de54645d7c4747ff072ff54e2b974ed878cea82e
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-05-12 16:44:51 +0200

    Add selecting posts to delete

--- src/ui/components/sd-table.ts
+++ src/ui/components/sd-table.ts
@@ -1,6 +1,8 @@
 import {editSdPost} from "./edit-sd-post";
 import {postLayout} from "./post-layout";
 import {packages, updateData, rowSdTable, sdInquiry, Threads} from "../../types/types";
+import {LocalStorageService} from "../../logic/local-storage-service";
+import {isUserForumMod} from "../../logic/helpers/tw-helper";
 
 
 export function sdTable(threads: Threads, updateData: updateData) {
@@ -11,7 +13,7 @@ export function sdTable(threads: Threads, updateData: updateData) {
     $(".text").css("max-width", "100%")
     console.log("thread id is in sd thread ids")
 
-
+    const localStorageService = LocalStorageService.getInstance();
     const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
     const edit_post_id: string | null = urlParams.get('edit_post_id');
     const currentThreadId: string = urlParams.get('thread_id') || ""
@@ -25,5 +27,19 @@ export function sdTable(threads: Threads, updateData: updateData) {
     }
     //display sd zeugs für alle nutzer
     //if admin or mod dann zu löschende Posts selecten und
+
+    if (isUserForumMod()) {
+        const sdPostId = threads[currentThreadId].sdPostId;
+        //find sd post
+        const sdPosts = $("a[name='" + sdPostId + "']").parent()
+        const postsToDelete = $(sdPosts).find("input[value=postCache]").siblings().find("span").text()
+        if (postsToDelete !== "") {
+            const postToDelete = postsToDelete.split(",")
+            postToDelete.forEach((postId: string) => {
+                $(`input[value=${postId}]`).prop("checked", true)
+                $(`input[value=${postId}]`).parent().parent().css("background", "rgba(174,6,6,0.73)")
+            })
+        }
+    }
 
 }

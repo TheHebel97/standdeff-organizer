1fb80126cddcc929046d8a21027b3d23c06fcb6c
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-09-16 19:30:21 +0200

    Add multiple paket rows possible

--- src/ui/new-thread.ts
+++ src/ui/new-thread.ts
@@ -12,9 +12,9 @@ export function createNewTable() {
             $(".clearfix > form > input[value=Senden]").attr("disabled", "true")
         }
     });
-   $(".clearfix > form > input[value=Senden]").on("mouseleave", function () {
-    $(".clearfix > form > input[value=Senden]").removeAttr("disabled");
-});
+    $(".clearfix > form > input[value=Senden]").on("mouseleave", function () {
+        $(".clearfix > form > input[value=Senden]").removeAttr("disabled");
+    });
 
 
     //debugger;
@@ -24,19 +24,22 @@ export function createNewTable() {
         const settingsBtn = `<input type="button" style="background-image: url(https://dsde.innogamescdn.com/asset/7b986b1a/graphic//buildings/smith.png);
             background-repeat: no-repeat; width:20px;height:20px; border: none; cursor: pointer; background-color: transparent; margin-left:7px;" class="configbtn">`;
         let tbBows = game_data.units.includes("archer") ? `<img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_archer@2x.png" title="Bogenschütze" alt="" class="" style="width: 15px; height: 15px">
-      <input type="text" class="inputBow" id="inputBow" style="width:70px;background-color:#EAD5AA">` : "";
-        let config = `<br><div class="configs" style="display: none; width: 95%;margin-left: auto;margin-bottom: 20px;
-      margin-right: auto;margin-top:15px;">
-      <img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_spear@2x.png" title="Speerträger" alt="" class="" style="width: 15px; height: 15px">
-      <input type="text" class="inputSpear" id="inputSpear" style="width:70px;background-color:#EAD5AA;margin-right: 10px"> 
-      <img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_sword@2x.png" title="Schwertkämpfer" alt="" class="" style="width: 15px; height: 15px"> 
-      <input type="text" class="inputSword" id="inputSword" style="width:70px;background-color:#EAD5AA;margin-right: 10px"> ` + tbBows + `
-      <br>
-      <br>
+      <input type="text" class="inputBow" style="width:70px;background-color:#EAD5AA">` : "";
+        let newPaketLineButton = `<input type="button" class="btn" id="newPaketLine" style="background-image: url('https://media.innogamescdn.com/TribalWars/emoji/274e.png'); background-size: cover; width: 15px; height: 15px; border: none; cursor: pointer; transform: rotate(45deg);">`;
 
-      <input type="button" class="btn" id="setupTable" value="SD Tabelle generieren">
-      </div>
-      `;
+        let configContent = `<br><img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_spear@2x.png" title="Speerträger" alt="" class="" style="width: 15px; height: 15px">
+<input type="text" class="inputSpear" style="width:70px;background-color:#EAD5AA;margin-right: 10px">
+<img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_sword@2x.png" title="Schwertkämpfer" alt="" class="" style="width: 15px; height: 15px">
+<input type="text" class="inputSword" style="width:70px;background-color:#EAD5AA;margin-right: 10px">` + tbBows +`
+<img src="https://dsde.innogamescdn.com/asset/b5d78b17/graphic/unit/unit_spy@2x.png" title="Späher" alt="" class="" style="width: 15px; height: 15px">
+<input type="text" class="inputSpy" style="width:70px;background-color:#EAD5AA;margin-right: 10px">` ;
+
+        let config = `<br><div class="configs" style="display: none; width: 95%;margin-left: auto;margin-bottom: 20px;
+  margin-right: auto;margin-top:15px;"><div class="inputCollection">` + configContent + newPaketLineButton + `</div>
+  <br>
+  <br>
+  <input type="button" class="btn" id="setupTable" value="SD Tabelle generieren">
+</div>`;
 
 
         //append html elements
@@ -44,24 +47,71 @@ export function createNewTable() {
         $(".clearfix > h2").append(config)
         $(".configbtn").on("click", swapConfgDisplay)
         $("#setupTable").on("click", newThread)
-        //++$("input[name=send]").on("click", function () {
+        $("#newPaketLine").on("click", function () {
+            $(".inputCollection").append(configContent);
 
+        });
 
     }
 
 }
 
+
 function newThread() {
     const localStorageService = LocalStorageService.getInstance();
     let troopArray = [];
-    // @ts-ignore
-    let spear = $("#inputSpear").val().split(",")
-    // @ts-ignore
-    let sword = $("#inputSword").val().split(",")
-    // @ts-ignore
-    let bow = game_data.units.includes("archer") ? $("#inputBow").val().split(",") : null;
-    troopArray.push(spear, sword, bow);
-    let paketText = createPaketString(troopArray)
+
+// Collect values from input elements with the class 'inputSpear'
+    let spear = $(".inputSpear").map(function () {
+        return $(this).val();
+    }).get();
+
+// Collect values from input elements with the class 'inputSword'
+    let sword = $(".inputSword").map(function () {
+        return $(this).val();
+    }).get();
+
+// Collect values from input elements with the class 'inputBow' if 'archer' is included in game_data.units
+    let bow = game_data.units.includes("archer") ? $(".inputBow").map(function () {
+        return $(this).val();
+    }).get() : null;
+
+    let spy = $(".inputSpy").map(function () {
+        return $(this).val();
+    }).get();
+
+    const allInputsEmpty = spear.every(val => val === "") && sword.every(val => val === "") && (!bow || bow.every(val => val === "")) && spy.every(val => val === "");
+
+    // Set default values if all inputs are empty
+    if (allInputsEmpty) {
+        spear = spear.map(() => "100");
+        sword = sword.map(() => "100");
+        if (bow) {
+            bow = bow.map(() => "100");
+        }
+        spy = spy.map(() => "5");
+    }
+
+    // Determine the maximum number of rows
+    let maxRows = Math.max(spear.length, sword.length, bow ? bow.length : 0, spy.length);
+
+    // Group values row by row
+    for (let i = 0; i < maxRows; i++) {
+        let row = [];
+        row.push(spear[i] || "");
+        row.push(sword[i] || "");
+        if (bow) {
+            row.push(bow[i] || "");
+        }
+        row.push(spy[i] || "");
+        troopArray.push(row);
+    }
+    console.log(troopArray)
+    troopArray = troopArray.filter(row => row.some(value => value !== ""));
+    console.log(troopArray)
+    let paketText = createPaketHtml(troopArray)
+    console.log("asdasd")
+    console.log(paketText)
     $(".clearfix > form > input[value=Senden]").on("click", function () {
         localStorageService.setNewThread = true;
 
@@ -72,7 +122,6 @@ function newThread() {
 [**]ID[||]Dorfkoordinaten[||]angefordert[||][color=#8d0100]noch benötigt[/color] [||]Spieler[||]Bemerkung[||]ab[||]bis[||][color=#001c83]Massen UT-Link[/color][/**]
 [/table]
 
-[b]1 Paket =[/b]
 ${paketText}
 
 [spoiler=Erklärung Paketsystem]Zur Vereinfachung des Standdeffschickens wird ein Paketsystem verwendet.
@@ -81,7 +130,7 @@ ${paketText}
 - Rechnung innerhalb des Skriptes einfacher :)
 - das Pflegen der Tabelle wäre auch ohne Skript einfacher
 
-Ein Paket besteht aus !einer! festen (hoffentlich sinnvollen @loserSfs: keine skav in sd splits) Anzahl an Einheiten. 
+Ein Paket besteht aus !einer! festen (hoffentlich sinnvollen @loserSfs: keine skav in sd splits) Anzahl an Einheiten.
 Dadruch entstehen im Stamm einheitliche Bunker und keine komischen Ausrutscher. Durch diese Art des Deffens können Verluste aufgeteilt und schneller nachgebaut werden.
 [/spoiler]
 
@@ -90,7 +139,7 @@ Das SD Skript besteht seit dem Update aus mehreren Bereichen:
 - Spieloptionen ganz unten -> Settings: Hier können die Einstellungen für das Skript vorgenommen werden
 - Massenunterstützung: Hierüber wird die Deff verschickt und das Skript (wenn eingeschaltet) trackt wieviele Einheiten benötigt werden und im Anschluss geschickt wurde.
 - SD Tabelle: Die Sd Tabelle ist die Übersicht, welche Auskunft über die Deffanfragen gibt. Es ist bei jedem Nutzer einzustellen in welchem Forum sich die SD Tabelle befindet. Dies kann über einen Button oder über ein Popup vermerkt werden.
-- Bearbeiten und Erstellen von Posts: 
+- Bearbeiten und Erstellen von Posts:
   ->über den Button "Bunker Anfragen" wird eine neue Anfrage erstellt. Diese wird innerhalb einem UI bearbeitet.
   ->über den Button "Bearbeitung eintragen" werden die bisher bearbeiteten Anfragen in das Nachrichtenfenster eingetragen.
 
@@ -132,23 +181,18 @@ Moin SFs:
     $("#message").val(text);
 }
 
-//todo: give multiple inputs instead of just spliiting by ","
-//fix typescript errors
-//rewrite manual
-const createPaketString = (troopArray: any[]) => {
+const createPaketHtml = (troopArray: any[]) => {
     let text = "";
-    // @ts-ignore
-    let paketRows = Math.max(parseInt(troopArray[0].length), parseInt(troopArray[1].length), game_data.units.includes("archer") ? parseInt(troopArray[2].length) : null)
-    for (let i = 0; i < paketRows; i++) {
-        let index = 0;
-        troopArray.forEach(element => {
-            text += index === 0 ? `[unit]spear[/unit][i]${element[i] === undefined ? "0" : element[i]}[/i] ` : "";
-            text += index === 1 ? `[unit]sword[/unit][i]${element[i] === undefined ? "0" : element[i]}[/i] ` : "";
-            text += index === 2 && game_data.units.includes("archer") ? `[unit]archer[/unit][i]${element[i] === undefined ? "0" : element[i]}[/i] ` : "";
-            index++;
-        })
-        text += "\n"
-    }
+    troopArray.forEach((row, index) => {
+        text += `[b]Paket ${index + 1}:[/b]\n`;
+        text += `[unit]spear[/unit] ${row[0] || 0} `;
+        text += `[unit]sword[/unit] ${row[1] || 0} `;
+        if (row.length > 3) {
+            text += `[unit]archer[/unit] ${row[2] || 0} `;
+        }
+        text += `[unit]spy[/unit] ${row[row.length - 1] || 0}\n `;
+        text += `\n`;
+    });
     return text;
 }
 

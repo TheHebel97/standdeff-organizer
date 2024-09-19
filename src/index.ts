203360470b01b2c38e3c9776412c8301af54aca2
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-09-19 11:27:40 +0200

    Refactor matching link

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         standdeff-organizer
+// @name         Development Script
 // @version      0.1
-// @description  Automatisiert das Organisieren von Standdeffs in Die Stämme
-// @author       TheHebel97
-// @match        https://*.die-staemme.de/game.php?village=*&screen=place&mode=call*
-// @match        https://*.die-staemme.de/game.php?village=*&screen=forum*
+// @description  try to take over the world!
+// @author       You, Fine, Regenmantel
+// @match        https://*.die-staemme.de/game.php?village=*&screen=place&*mode=call*
+// @match        https://*.die-staemme.de/game.php?*village=*&screen=forum*
 // @match        https://*.die-staemme.de/game.php?*village=*&screen=settings*
 // @grant        GM_getValue
 // @grant        GM_setValue

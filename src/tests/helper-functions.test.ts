c210b455094a911261054f87dcd61577badef0a4
TheHebel97 <50582846+TheHebel97@users.noreply.github.com>
2024-09-19 11:48:11 +0200

    Remove tests

--- src/tests/helper-functions.test.ts
+++
@@ -1,8 +0,0 @@
-const helperFunctions = require('../logic/helpers/helper-functions')
-
-test('storeDataInGM', () => {
-    const data : object = {test: "test"};
-    const key = "test";
-    helperFunctions.storeDataInGM(data, key);
-    expect(helperFunctions.getDataFromGM(key)).toEqual(data);
-});
